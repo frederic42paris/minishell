18b61d36b73baf3b55624497e4d2336a1a691af9
Robin Richard Canavaggio <robin.richard-cana@hotmail.fr>
2024-07-22 14:50:25 +0200

    added exit builtin, all builtins are correct

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,35 +6,35 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 19:11:30 by sumseo            #+#    #+#             */
-/*   Updated: 2024/07/19 22:47:04 by rrichard         ###   ########.fr       */
+/*   Updated: 2024/07/22 14:49:53 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	control_alpha(char *s, t_parse *cmds_list)
+t_bool	ft_check_args(char *str)
 {
-	printf("exit: %s: numeric argument required\n", s);
-	if (cmds_list->old_stdin != -1)
-		close(cmds_list->old_stdout);
-	if (cmds_list->old_stdout != -1)
-		close(cmds_list->old_stdin);
-	exit(1);
-}
+	int	count_digits;
 
-// void	control_many_args(t_parse *cmds_list)
-// {
-// 	printf("exit : too many arugments\n");
-// 	if (cmds_list->old_stdin != -1)
-// 		close(cmds_list->old_stdout);
-// 	if (cmds_list->old_stdout != -1)
-// 		close(cmds_list->old_stdin);
-// 	exit(1);
-// }
+	count_digits = 0;
+	if (ft_strtol(str, NULL, 10) > INT_MAX || ft_strtol(str, NULL, 10) < INT_MIN)
+		return (FALSE);
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+	{
+		count_digits++;
+		str++;
+	}
+	if (*str != '\0' || count_digits > 10)
+		return (FALSE);
+	return (TRUE);
+}
 
 void	normal_exit(t_parse *cmds_list, int exit_code)
 {
-	printf("exit\n");
 	if (cmds_list->old_stdin != -1)
 		close(cmds_list->old_stdout);
 	if (cmds_list->old_stdout != -1)
@@ -44,22 +44,29 @@ void	normal_exit(t_parse *cmds_list, int exit_code)
 
 void	func_exit(t_parse *cmds)
 {
-	char	*endptr;
-	long	exit_code;
+	int		num_args;
 
-	if (!cmds->cmd_array[1])
-		normal_exit(cmds, EXIT_SUCCESS);
-	if (cmds->cmd_array[1] && cmds->cmd_array[2])
-		return ((void)printf("exit: too many arguments\n"));
-	exit_code = ft_strtol(cmds->cmd_array[1], &endptr, 10);
-	if (errno == ERANGE && (exit_code == LONG_MAX || exit_code == LONG_MIN))
-		printf("exit: %s: numeric argument required\n", cmds->cmd_array[1]);
-	else if (errno != 0 && exit_code == 0)
-		printf("exit: %s: numeric argument required\n", cmds->cmd_array[1]);
-	else if (endptr == cmds->cmd_array[1])
-		printf("exit: %s: numeric argument required\n", cmds->cmd_array[1]);
-	else if (*endptr != '\0')
-		printf("exit: %s: numeric argument required\n", cmds->cmd_array[1]);
+	num_args = 0;
+	while (cmds->cmd_array[num_args])
+		num_args++;
+	if (cmds->cmd_array[1])
+	{
+		if (!ft_check_args(cmds->cmd_array[1]))
+		{
+			printf("exit: %s: numeric argument required\n", cmds->cmd_array[1]);
+			normal_exit(cmds, 2);
+		}
+		else if (num_args > 2)
+			return ((void)printf("exit: too many arguments\n"));
+		else
+		{
+			printf("exit\n");
+			normal_exit(cmds, ft_atoi(cmds->cmd_array[1]) % 256);
+		}
+	}
 	else
-		normal_exit(cmds, (int)(exit_code % 256));
+	{
+		printf("exit\n");
+		normal_exit(cmds, 0);
+	}
 }
