23af316e23d45b371a07e8f81d1168965d66b81a
Frederic Tanon <ftanon@paul-f3Br7s4.clusters.42paris.fr>
2024-07-30 18:47:49 +0200

    norm

--- parsing/store_command.c
+++ parsing/store_command.c
@@ -6,7 +6,7 @@
 /*   By: ftanon <ftanon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 16:01:57 by ftanon            #+#    #+#             */
-/*   Updated: 2024/07/30 17:29:34 by ftanon           ###   ########.fr       */
+/*   Updated: 2024/07/30 18:47:32 by ftanon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,24 @@ int	count_words_meta(t_token *tok_list)
 	return (len);
 }
 
+int	init_cmd_array(t_token *tok_list, t_parse *par_list)
+{
+	int	len;
+
+	len = count_words_meta(tok_list);
+	free_array(par_list->cmd_array);
+	par_list->cmd_array = (char **)malloc(sizeof(char *) * (len + 1));
+	if (par_list->cmd_array == NULL)
+		return (1);
+	return (0);
+}
+
 int	split_command(t_token *tok_list, t_parse *par_list)
 {
-	int	i;
 	int	j;
 
 	j = 0;
-	i = count_words_meta(tok_list);
-	free_array(par_list->cmd_array);
-	par_list->cmd_array = (char **)malloc(sizeof(char *) * (i + 1));
-	if (par_list->cmd_array == NULL)
+	if (init_cmd_array(tok_list, par_list))
 		return (1);
 	while (tok_list)
 	{
