e732aa6b3f79a63bb03be7ca42ce8c40a89f0a7e
Frederic Tanon <ftanon@paul-f3Br7s4.clusters.42paris.fr>
2024-07-30 14:25:30 +0200

    norm change

--- check_error/check_input.c
+++ check_error/check_input.c
@@ -6,47 +6,57 @@
 /*   By: ftanon <ftanon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 11:37:19 by ftanon            #+#    #+#             */
-/*   Updated: 2024/07/28 17:12:11 by ftanon           ###   ########.fr       */
+/*   Updated: 2024/07/30 14:23:40 by ftanon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	check_input(char const *str)
+int	check_empty_string(char const *str)
 {
 	int	i;
 
 	i = 0;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '\0')
+		return (1);
+	return (0);
+}
+
+int	check_quote_unclosed(char const *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '"')
+		if (str[i] == '"' || str[i] == 39)
 		{
+			c = str[i];
 			i++;
-			while (not_double_quote(str[i]))
+			while (str[i] != c && str[i] != '\0')
 				i++;
 			if (str[i] == '\0')
 			{
-				printf("Error : Double quotes unclosed\n");
-				return (1);
-			}
-		}
-		else if (str[i] == 39)
-		{
-			i++;
-			while (not_single_quote(str[i]))
-				i++;
-			if (str[i] == '\0')
-			{
-				printf("Error : Single quotes unclosed\n");
+				if (c == '"')
+					printf("Error : Double quotes unclosed\n");
+				else if (c == 39)
+					printf("Error : Single quotes unclosed\n");
 				return (1);
 			}
 		}
 		i++;
 	}
-	i = 0;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '\0')
+	return (0);
+}
+
+int	check_input(char const *str)
+{
+	if (check_quote_unclosed(str) == 1)
+		return (1);
+	if (check_empty_string(str) == 1)
 		return (1);
 	return (0);
 }
