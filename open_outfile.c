b45eac75dd64c2c2b43eced68410a67fb50f9d6b
rrichard <rrichard@student.42.fr>
2024-08-08 17:34:08 +0200

    now opens the right outfile

--- open_outfile.c
+++ open_outfile.c
@@ -6,7 +6,7 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 17:38:41 by ftanon            #+#    #+#             */
-/*   Updated: 2024/08/08 15:26:43 by rrichard         ###   ########.fr       */
+/*   Updated: 2024/08/08 17:32:08 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,26 @@
 
 int	open_outfile(t_parse *par_list, t_data *data)
 {
+	t_redir	*current_redir;
+
 	while (par_list->next)
 		par_list = par_list->next;
-	if (!par_list->redirection)
-	{
-		data->fd_stdout = STDOUT_FILENO;
+	current_redir = par_list->redirection;
+	data->fd_stdout = STDOUT_FILENO;
+	if (!current_redir)
 		return (0);
-	}
-	while (par_list->redirection->next)
-		par_list->redirection = par_list->redirection->next;
-	if (par_list->redirection->type == 1)
+	while (current_redir->next)
+		current_redir = current_redir->next;
+	if (current_redir->type == 1)
 	{
-		if (!ft_strcmp(par_list->redirection->token, ">"))
-			data->fd_stdout = open(par_list->redirection->name,
+		if (!ft_strcmp(current_redir->token, ">"))
+			data->fd_stdout = open(current_redir->name,
 					O_CREAT | O_TRUNC | O_WRONLY, 0644);
-		else if (!ft_strcmp(par_list->redirection->token, ">>"))
-			data->fd_stdout = open(par_list->redirection->name,
+		else if (!ft_strcmp(current_redir->token, ">>"))
+			data->fd_stdout = open(current_redir->name,
 					O_CREAT | O_APPEND | O_WRONLY, 0644);
 		if (data->fd_stdout < 0)
-			return (perror(par_list->redirection->name), 1);
+			return (perror(current_redir->name), 1);
 	}
-	if (data->fd_stdout == 0)
-		data->fd_stdout = STDOUT_FILENO;
 	return (0);
 }
